12e3392fd0438d5a77c8f53f6d820f196f8b3908
baiju <baiju@thehelloworld.com>
2020-06-17 14:06:36 +0530

    testing

--- repyute/assets/js/script.js
+++ repyute/assets/js/script.js
@@ -39,38 +39,3 @@ function sendContact() {
   }
 }
 
-function validateContact() {
-  var valid = true
-  $('.demoInputBox').css('background-color', '')
-  $('.info').html('')
-  if (!$('#userName').val()) {
-    $('#userName-info').html('(required)')
-    $('#userName').css('background-color', '#FFFFDF')
-    valid = false
-  }
-  if (!$('#userEmail').val()) {
-    $('#userEmail-info').html('(required)')
-    $('#userEmail').css('background-color', '#FFFFDF')
-    valid = false
-  }
-  if (
-    !$('#userEmail')
-      .val()
-      .match(/^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/)
-  ) {
-    $('#userEmail-info').html('(invalid)')
-    $('#userEmail').css('background-color', '#FFFFDF')
-    valid = false
-  }
-  if (!$('#subject').val()) {
-    $('#subject-info').html('(required)')
-    $('#subject').css('background-color', '#FFFFDF')
-    valid = false
-  }
-  if (!$('#content').val()) {
-    $('#content-info').html('(required)')
-    $('#content').css('background-color', '#FFFFDF')
-    valid = false
-  }
-  return valid
-}
